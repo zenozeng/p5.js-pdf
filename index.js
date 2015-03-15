cf83e0db1d0774fd7198b8457e359202381523b2
Zeno Zeng <zenoofzeng@gmail.com>
2015-03-15 19:54:56 +0800

    add doc

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /**
- * p5.js-pdf
+ * p5.js-pdf - Simple PDF module for p5.js using jsPDF API
  * Copyright (c) 2015 Zeno Zeng<zenoofzeng@gmail.com>.
- * Licensed under the MIT License.
  *
- * Simple jsPDF API warpper for p5.js
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 (function(p5) {
@@ -12,6 +27,15 @@
 
     var jsPDF = require('./jspdf/index');
 
+    /**
+     * Constructor
+     *
+     * @param {Object} options - The options for p5.PDF instance
+     * @param {Canvas} options.canvas - The <canvas> to capture, defaults to document.getElementById('defaultCanvas')
+     * @param {Number} options.ppi - The ppi for frames
+     * @param {String} options.imageType - Use which imageType, defaults to JPEG.
+     * @return {p5.PDF} a p5.PDF instance
+     */
     var PDF = function(options) {
         if(!options) {
             options = {};
@@ -30,7 +54,12 @@
         this.yOffset = 0;
     };
 
-    // manually capture current canvas
+    /**
+     * Capture current frame
+     *
+     * Convert canvas to image and save it in pdf,
+     * and will open new page automatically if necessary.
+     */
     PDF.prototype.capture = function() {
         var img = this.canvas.toDataURL('image/' + this.imageType, 0.95);
 
@@ -61,13 +90,22 @@
         this.yOffset += height;
     };
 
-    // go to nextpage
+    /**
+     * Open new page
+     *
+     */
     PDF.prototype.nextPage = function() {
         this.yOffset = 0;
         this.pdf.addPage();
     };
 
     // must be called onclick otherwise will be prevented by browser
+
+    /**
+     * Save current PDF
+     *
+     * @param {String} filename - Filename for your pdf file, defaults to untitled.pdf
+     */
     PDF.prototype.save = function(filename) {
         filename = filename || "untitled.pdf";
         var a = document.createElement('a');
@@ -80,13 +118,23 @@
         }, 0);
     };
 
-    // Convert to Object URL using URL.createObjectURL
-    // 适合用于在页面内直接显示 PDF
+    /**
+     * Generate a object url for current PDF
+     *
+     * @return {String} objectURL
+     */
     PDF.prototype.toObjectURL = function() {
         return this.pdf.output('bloburi');
     };
 
-    // convert to data url
+    /**
+     * Generate a data url for current PDF
+     *
+     * Note that you should always use toObjectURL if possible,
+     * generating dataurl for large pdf is very expensive.
+     *
+     * @return {String} dataurl
+     */
     PDF.prototype.toDataURL = function() {
         return this.pdf.output('datauristring');
     };
