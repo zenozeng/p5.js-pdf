b414e92e0e2f4b5988a651ad30cf76d530438d27
Zeno Zeng <zenoofzeng@gmail.com>
2015-06-19 15:15:44 +0800

    print = function(html, styles)

--- demo/print/print.js
+++ demo/print/print.js
@@ -1,38 +1,36 @@
-var printPage = function() {
-    var status = {
-        focused: window.focused
+(function() {
+    // string html
+    // string styles
+    var print = function(html, styles) {
+        // note that window.print might be overridden by p5.js
+        var iframe = document.createElement("iframe");
+        iframe.height = 0;
+        iframe.width = 0;
+        document.body.appendChild(iframe);
+        var doc = iframe.contentDocument || iframe.contentWindow.documen;
+        var win = iframe.contentWindow;
+        var style = doc.createElement('style');
+        style.innerHTML = styles;
+        doc.body.appendChild(style);
+        var div = doc.createElement('div');
+        div.innerHTML = html;
+        doc.body.appendChild(div);
+        iframe.contentWindow.print.call(win);
     };
-    console.log(status);
 
-    // note that window.print might be overridden by p5.js
-    var iframe = document.createElement("iframe");
-    iframe.height = 0;
-    iframe.width = 0;
-    document.body.appendChild(iframe);
-    iframe.contentWindow.print.call(window, "foo");
+    window.setup = function() {
+        createSVG(1000, 1000);
+        fill(200);
+    };
 
-    // //workaround for Chrome bug - https://code.google.com/p/chromium/issues/detail?id=141633
-    // if (window.stop) {
-    //     window.location.reload(); //triggering unload (e.g. reloading the page) makes the print dialog appear
-    //     window.stop(); //immediately stop reloading
-    // }
+    window.draw = function() {
+        ellipse(500, 500, 500, 500);
+        noLoop();
+    };
 
-    return false;
-};
-
-function setup() {
-    createSVG(1000, 1000);
-    fill(200);
-
-    printPage();
-}
-
-function draw() {
-    ellipse(500, 500, 500, 500);
-    noLoop();
-    // printPage();
-}
-
-function mouseClicked() {
-    printPage();
-}
+    window.mouseClicked = function() {
+        var svg = document.querySelector('svg');
+        svg = (new XMLSerializer()).serializeToString(svg);
+        print(svg, '@page { size: 100mm 100mm; }');
+    };
+})();
