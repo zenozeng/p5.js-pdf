eedc5d0ac7171d71c671fa49b8ce0dd8fa29ec29
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-13 20:19:07 +0800

    remove unnecessary files

--- jspdf/index.js
+++
@@ -1,9 +0,0 @@
-var jsPDF = require('./jspdf');
-
-// load addImage plugin and jsPDF API
-require('./jspdf.plugin.addimage');
-
-// load PNG plugin and update jsPDF API
-require('./jspdf.plugin.png_support');
-
-module.exports = jsPDF;
