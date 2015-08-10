8251070a0b5a8e36cb837853b2313a10f3d8c611
Zeno Zeng <zenoofzeng@gmail.com>
2015-08-10 09:20:55 +0800

    add examples/vector

---
+++ examples/examples/vector.js
@@ -0,0 +1,21 @@
+var pdf;
+
+function setup() {
+    // If we use SVG Renderer, then the PDF generated will be vector
+    // Note that to use SVG Renderer, you must include p5.svg library
+    createCanvas(200, 200, SVG);
+    pdf = createPDF();
+    pdf.beginRecord();
+}
+
+function draw() {
+    background(255);
+    fill('#ED225D');
+    textSize(50);
+    textAlign(CENTER);
+    text(frameCount, width / 2, height / 2);
+    if (frameCount == 100) {
+        noLoop();
+        pdf.save();
+    }
+}
