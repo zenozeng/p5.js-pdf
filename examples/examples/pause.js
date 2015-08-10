0b1b1e8f05b9aca074370297a33bad4f2f075baf
Zeno Zeng <zenoofzeng@gmail.com>
2015-08-10 10:14:41 +0800

    add examples/pause

---
+++ examples/examples/pause.js
@@ -0,0 +1,28 @@
+var pdf;
+
+function setup() {
+    // If we use SVG Renderer, then the PDF generated will be vector
+    // Note that to use SVG Renderer, you must include p5.svg library
+    createCanvas(600, 200, SVG);
+    pdf = createPDF();
+    pdf.beginRecord();
+}
+
+function draw() {
+    background(255);
+    fill('#ED225D');
+    textSize(100);
+    textAlign(CENTER);
+    text(frameCount, width * 0.5, height * 0.5);
+    if (frameCount == 3) {
+        pdf.endRecord();
+    }
+    if (frameCount == 7) {
+        pdf.beginRecord();
+    }
+    if (frameCount == 10) {
+        noLoop();
+        pdf.save();
+    }
+    pdf.nextPage();
+}
